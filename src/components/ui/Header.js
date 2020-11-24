113d5c6df2a649af36820b693f2732a6dadd96ba
Anu Singh <anusingh1883656@gmail.com>
2020-11-24 11:19:23 +0530

    react-bad-cast

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../images/logo.png"
+import "../../App.css"
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={logo} alt="logo"></img>
+        </header>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-import Application from './Application';
 
-import './styles.css';
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(<Application />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
